89ecd1350c6cc41224cd785c3718be5288e96143
c22018086 <rafik.chaib@etu.univ-amu.fr>
2023-03-26 23:30:39 +0200

    fixing the app.listen

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use((err, req, res, next) => {
 });
 
 // bot up the server .
-const port = process.env.PORT || 3000
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log(`app listening on port ${port}`);
 });
