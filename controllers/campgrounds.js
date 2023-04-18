6f4094dadebbc3847c0fbc2ca2409bc482d72b88
c22018086 <rafik.chaib@etu.univ-amu.fr>
2023-04-18 16:56:48 +0200

    adding fixing the geolocalisation for the second time

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -58,21 +58,16 @@ module.exports.edit = async (req, res) => {
     console.log("body from edit", req.body);
     const { id } = req.params;
 
+   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground}); // I used destructuring just for fun .
+    const images = req.files.map(file => ({ url: file.path, filename: file.filename }));
+    campground.images.push(...images);
+    
     const response = await geocoder.forwardGeocode({
         query: req.body.campground.location,
         limit: 1
     }).send();
-    let campground = req.body.campground;
     // adding the geojson object provided by the mapbox api , into the model 
     campground.geometry = response.body.features[0].geometry;
-    campground.author = req.user._id;
-    campground.images = req.files.map(file => ({ url: file.path, filename: file.filename }));
-
-    campground = await Campground.findByIdAndUpdate(id, { ...campground}); // I used destructuring just for fun .
-
-    const images = req.files.map(file => ({ url: file.path, filename: file.filename }));
-    campground.images.push(...images);
-
 
     if (req.body.deleteImages) {
         for (let filename of req.body.deleteImages) {
